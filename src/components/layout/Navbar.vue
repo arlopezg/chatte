0f9dacda6d2fa6bc975a611fa2a27ec2e888c52b
Alejandro López <alejandrolopez.gd@gmail.com>
2021-07-23 00:58:21 -0400

    Update Navbar.vue

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -10,7 +10,7 @@
       <div class="text-sm lg:flex-grow"></div>
       <div>
         <a
-          href="https://github.com/arlopezg/cognite-chat-demo"
+          href="https://github.com/arlopezg/chatte"
           target="_blank"
           class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-blue-500"
           >Check the code</a
